512c3a395baa2764207ac42e44bd4b1aa1494502
Tomas Myklebust <Tomas.Myklebust@stud.noroff.no>
2023-03-12 19:25:14 +0100

    javascript, incomplete does not run

--- js/script.js
+++ js/script.js
@@ -1,11 +1,53 @@
-const url ="https://tmprojects.no/projectexam/wp-json/wp/v2/posts/?_embed&acf_format=standard&per_page=10"
+const baseUrl ="https://tmprojects.no/projectexam/wp-json/wp/v2/posts/?_embed&acf_format=standard&per_page=10"
 const postContainer = document.querySelector(".postContainer");
 const carousel = document.querySelector(".carouselContainer");
 const moreButton = document.querySelector(".button_viewMore");
 const error = document.querySelector(".error");
 
 
-async function getPosts(){
+async function getPosts (url) {
+    const data = await fetch(url);
+    const result = await data.json();
+    console.log(result);
+
+    
+
+    postContainer.innerHTML = "";
+    for (i = 0 ; i < result.length; i++) {
+        const id = result[i].id;
+        const image = result[i]._embedded["wp:featuredmedia"][0];
+        const title = result[i].title.rendered;
+        const previousText = result[i].excerpt.rendered;
+
+
+        if(result.length < 10) {
+            nextPosts.style.display = "none";
+        }  
+
+        postContainer.innerHTML += 
+        `<div class="post">
+        <a href="detail.html?id=${id}">
+        <h2>${title}</h2>
+        <img src="${image}" alt="image of ${title}" />
+        <p>${previousText}</p>
+        </a>
+        </div>`
+        ;
+       
+    };
+}
+ 
+getPosts(baseUrl);
+
+
+
+
+
+
+
+
+
+/*async function getPosts(){
     
     const response = await fetch(url);
     const results = await response.json();
@@ -16,8 +58,8 @@ async function getPosts(){
         postContainer.innerHTML +=
         `<div class="postHeading">
         <h2>${results[i].title.rendered}</h2></div>
-        <img src=${results[i]_embedded.['wp:featuredmedia']['0'].source_url}/>
+        <img src=${results[i]_embedded.['wp:featuredmedia']['0'].media_details.sizes.medium.source_url}/>
     }
             `
     }}
-        getPosts(url);
+        getPosts(url);*/

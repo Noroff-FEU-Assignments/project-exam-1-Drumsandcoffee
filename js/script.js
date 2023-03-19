008b1bb050947e23ac78ba4783c5c68be7bb5b43
Drumsandcoffee <Tomas.Myklebust@stud.noroff.no>
2023-03-19 22:44:15 +0100

    css and small changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const url ="https://tmprojects.no/projectexam/wp-json/wp/v2/posts/?_embed&acf_format=standard&per_page=10"
+const baseUrl ="https://tmprojects.no/projectexam/wp-json/wp/v2/posts/?_embed&acf_format=standard&per_page=10"
 const postContainer = document.querySelector(".postContainer");
 const carousel = document.querySelector(".carouselContainer");
 const moreButton = document.querySelector(".button_viewMore");
@@ -7,17 +7,19 @@ const error = document.querySelector(".error");
 
 async function getPosts(){
     
-    const response = await fetch(url);
+    const response = await fetch(baseUrl);
     const results = await response.json();
     console.log(results)
-    console.log(results[0].embedded.wpfeaturedmedia);
+    console.log(results[0]._embedded['.wpfeaturedmedia']);
 
     for (let i = 0; i < results.length; i++) {
+        const id = results[0].guid.id;
         postContainer.innerHTML +=
-        `<div class="postHeading">
+        `<a href="detail.html?id=${id}">
+        <div class="postHeading">
         <h2>${results[i].title.rendered}</h2></div>
-        <img src=${results[i]_embedded.['wp:featuredmedia']['0'].source_url}/>
-    }
-            `
+        <div class="postText"> ${results[i].excerpt.rendered}</div>
+        <img src="${results[i]._embedded['wp:featuredmedia'][0].source_url}"/>
+    `
     }}
-        getPosts(url);
+        getPosts(baseUrl);

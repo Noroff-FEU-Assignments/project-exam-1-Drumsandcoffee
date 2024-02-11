d2078b01a7be0eb73f4178b999f567a0350f3be8
Drumsandcoffee <Tomas.Myklebust@stud.noroff.no>
2024-02-11 20:08:53 +0100

    js css changes
    changes to script and styles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //const baseUrl ="http://localhost/ProjectExam/wordpress-6.1.1/wordpress/wp-json/wp/v2/posts/"
 
 let page = 1;
-const baseUrl = `http://localhost/ProjectExam/wordpress-6.1.1/wordpress/wp-json/wp/v2/posts/`
+const baseUrl = `http://localhost/ProjectExam/wordpress-6.1.1/wordpress/wp-json/wp/v2/posts?per_page=10&_embed`
 const carousel = document.querySelector(".carouselContainer");
 const moreButton = document.querySelector(".button_viewMore");
 const error = document.querySelector(".error");
@@ -9,6 +9,7 @@ const error = document.querySelector(".error");
 
 
 const postContainer = document.querySelector(".postContainer");
+const displayPost = document.querySelector(".display_post");
 const latestPostsContainer = document.querySelector("latestPosts_container");
 const morePostsButtonContainer = document.querySelector(".more_posts_button_container");
 const morePostsButton = document.querySelector(".more_posts_button");
@@ -31,18 +32,18 @@ async function getPosts(url){
 
     for (let i = 0; i < results.length; i++) {
         console.log(results[i].title.rendered)
-        
         const id = results[0].guid.id;
         postContainer.innerHTML +=
         `
+        
         <div class="postHeading">
         <h2>${results[i].title.rendered}</h2></div>
         <div class blog_content>
         <p>${results[i].content.rendered}</p>
         </div>
         
-    }
-            `
+        
+    
+        `   
     }}
         getPosts();
-//<a href="detail.html?id=${id}"></a>
